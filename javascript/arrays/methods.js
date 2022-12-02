7bab31dc7f9aa18f2f5b6045258252b4e98c9d69
Estar Guan <estarguan@Estars-MacBook.local>
2022-12-01 21:31:33 -0500

    I added a console log

--- javascript/arrays/methods.js
+++ javascript/arrays/methods.js
@@ -4,4 +4,5 @@ const numberArray = [1,2,3,4,5,6,7,8,9,10];
 const squaredArray = numberArray.map((element, index, originalArray) => element*index);
 
 console.log(squaredArray);
+console.log("hello");
 
