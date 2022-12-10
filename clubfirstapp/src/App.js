bedf593e03f4551d1b10c332b8d498a32850ef84
Estar Guan <estarguan1111@gmail.com>
2022-12-10 16:43:19 -0500

    trying to set up a router

--- clubfirstapp/src/App.js
+++ clubfirstapp/src/App.js
@@ -1,18 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-import Box from './components/Box.js'
+// import { useEffect } from 'react';
 
-function App() {
+//import logo from './logo.svg';
+import './App.css';
+//import YoutubeLink from './components/YotubeLink.js';
+import FormSubmitted from './pages/FormSubmitted';
+import {Route, BrowserRouter as Router} from "react-router-dom";
+
+function App(){
+  return(
+    //<Router>
+      
+    <div className="App">
+      <body>
+        <h1>Entrance form</h1>
+        <form>
+          <fieldset >
+            <legend>Personal Info</legend>
+            <label for = "First Name">First Name: </label><input id="First Name" placeholder="First Name" type="text"/>
+            <br/>
+            <label for ="Last Name">Last Name:</label> <input id = "Last Name" placeholder="Last Name" type="text"/>
+          </fieldset>
+          <fieldset>
+            <legend>Login Info</legend>
+            <label>Username:</label> <input placeholder="Username" type = "text"/><br/>
+            <label>Password:</label> <input placeholder="Password" type = "password"/><br/>
+            <label>Date of Birth:</label> <input type="date"/>
+          </fieldset>
+        </form>
+        <button onClick={() => window.location.replace("https://www.w3schools.com")}>Submit</button>
+      </body>
+    </div>
+    //</Router>
+  )
+}
+
+export default App;
+
+
+/*const App = () =>  {
+  // useEffect(() => {
+  //   fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  //   .then(response => response.json())
+  //   .then(allpokemon => console.log(allpokemon))
+  // }, [])
+  // let youtubeLink = 'https://www.youtube.com/watch?v=8i_FOk955c0';
+  // youtubeLink='https://google.com'
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> welcome to the website</p>
-        <Box/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        > 
+        </a>
+        <YoutubeLink link="https://www.youtube.com/"/>
       </header>
-      
     </div>
   );
-}
-
-export default App;
+}*/
---
+++ clubfirstapp/src/pages/FormSubmitted.js
@@ -0,0 +1,8 @@
+function FormSubmitted(){
+    return(
+        <div>
+            <h1>Your Form has been submitted</h1>
+        </div>
+    )
+}
+export default FormSubmitted;
