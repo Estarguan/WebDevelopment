41089dfe324fb8e0b070af3e5036be91a2e1adbe
Estar Guan <estarguan1111@gmail.com>
2022-12-24 11:07:24 -0500

    made a butt ton of changes

---
+++ calculator/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div>
+      <section className = "App-title">
+      <h1>Enjoy our free calculator</h1>
+      </section>
+      <section>
+        <Calculator/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
--- clubfirstapp/src/pages/FormSubmitted.js
+++
@@ -1,8 +0,0 @@
-function FormSubmitted(){
-    return(
-        <div>
-            <h1>Your Form has been submitted</h1>
-        </div>
-    )
-}
-export default FormSubmitted;
