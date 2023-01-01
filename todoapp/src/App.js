56ba67e8d4ff3a3036e372a20eefab179c0da86f
Estar Guan <estarguan1111@gmail.com>
2023-01-01 18:49:19 -0500

    fixed issues

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -7,85 +7,6 @@ import Main  from './pages/main';
 
 
 function App() {
-<<<<<<< HEAD
-  const [list, setList] = useState(false);
-  const [isShown, setIsShown] = useState(false);
-  const [doList, setDoList] = useState([]);
-  const [editingIndex, setEditingIndex] = useState(-1);
-  //const [removalIndex, setRemovalIndex] = useState(-1);
-
-  const add = () => {
-    setIsShown(true);
-  };
-
-  const listupdate = (event) => {
-    if (event.key === 'Enter') {
-      if (event.target.value !== '') {
-        doList.push(event.target.value);
-        setDoList(doList);
-        setList(true);
-        setIsShown(false);
-        event.target.value = '';
-      }
-    }
-  };
-
-  const saveChanges = (event) => {
-    if (event.key === 'Enter') {
-      if (event.target.value !== '') {
-        const updatedDoList = [...doList];
-        updatedDoList[editingIndex] = event.target.value;
-        setDoList(updatedDoList);
-        setEditingIndex(-1);
-        event.target.value = ''
-      }
-    }
-  };
-
-  const remove = (index) => {
-    const updatedDoList = [...doList];
-    updatedDoList.splice(index,1)
-    setDoList(updatedDoList)
-  }
-  return (
-    <div className="App">
-      <h1>Welcome to my to do app</h1>
-      <section>
-        <button onClick={add} className="Add">
-          +
-        </button>
-      </section>
-      <input
-        placeholder="Enter Your Task"
-        className="input"
-        style={{ display: isShown ? 'block' : 'none' }}
-        type="text"
-        onKeyPress={listupdate}
-      ></input>
-      
-      {/* editing */}
-      <input
-        placeholder="Edit task"
-        className="input"
-        style={{ display: editingIndex !== -1 ? 'block' : 'none' }}
-        type="text"
-        onKeyPress={saveChanges}
-      ></input>
-      
-      {/* displaying */}
-      <ol style={{ display: list ? 'inline' : 'none' }}>
-        {doList.map((item, i) => (
-          <li key={i}>
-            <input type='checkbox'></input>
-            {item}
-            {/* editing button */}
-            <button className='edit-button' onClick={() => setEditingIndex(i)}>Edit</button>
-            <button className='edit-button' onClick={() => {remove(i)}}>Remove</button>
-          </li>
-        ))}
-      </ol>
-    </div>
-=======
   return (
     <>
       <NavBar/>
@@ -95,7 +16,6 @@ function App() {
         <Route path = 'Contact' element={<Contact/>}/>
       </Routes>
     </>
->>>>>>> refs/remotes/origin/main
   );
 }
 
