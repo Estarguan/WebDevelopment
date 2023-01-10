2057f14e565be9039a2ac174cd0c849aa4ae864e
Estar Guan <estarguan1111@gmail.com>
2023-01-09 22:54:16 -0500

    finished the flask python react tuturiol thing

---
+++ reactFlaskTut/client/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/members")
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+        console.log(data);
+      });
+  },[]);
+  return <div>app</div>;
+}
+
+export default App;
