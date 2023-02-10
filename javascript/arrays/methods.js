67b2c0838561f571823f9901525a184a4221922a
Estar Guan <estarguan1111@gmail.com>
2023-02-09 19:04:14 -0500

    made some simple changes

--- javascript/arrays/methods.js
+++
@@ -1,8 +0,0 @@
-//map
-const numberArray = [1,2,3,4,5,6,7,8,9,10];
-
-const squaredArray = numberArray.map((element, index, originalArray) => element*index);
-
-console.log(squaredArray);
-const string = "Estar";
-console.log(string);
